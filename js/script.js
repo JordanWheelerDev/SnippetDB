f34af8398246d7257a5cbe354e304d918dbbeb6d
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-05-11 20:30:25 -0230

    Added Get Coding template

--- js/script.js
+++ js/script.js
@@ -88,6 +88,9 @@ document.addEventListener("DOMContentLoaded", () => {
         case "react":
             searchInput.placeholder = "What is useReducer() hook in React?";
             break;
+        case "css":
+            searchInput.placeholder = "What are element selectors in CSS?";
+            break;
           default:
             searchInput.placeholder = "Enter your search query";
         }
